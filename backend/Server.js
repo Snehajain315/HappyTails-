83f77299b7b050fd4e10f9f4e9ddd79b00e8bdde
Sneha Jain <Snehajain315@gmail.com>
2025-10-31 13:01:59 +0530

    fix port

--- backend/Server.js
+++ backend/Server.js
@@ -32,5 +32,5 @@ app.use("/uploads", express.static("uploads"));
 
 //Listen:-
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
