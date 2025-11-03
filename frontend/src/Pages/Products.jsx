08e5ed62a33fe295c4c3c91fc5a451516976e504
Sneha Jain <Snehajain315@gmail.com>
2025-11-03 15:54:42 +0530

    url change

--- frontend/src/Pages/Products.jsx
+++ frontend/src/Pages/Products.jsx
@@ -10,7 +10,7 @@ export default function CategoryProducts({setCartData, setWishListData}) {
   useEffect(() => {
     async function fetchProducts() {
       try {
-        const res = await axios.get(" https://happytails-wkk8.onrender.com/api/product/",{
+        const res = await axios.get("https://happytails-wkk8.onrender.com/api/product/",{
           withCredentials: true
         });
         const filtered = res.data.filter((item) => item.category === slug);
