186635350a83284a1c46f2eebc8fe358ee27b9f8
Sneha Jain <Snehajain315@gmail.com>
2025-11-04 16:15:43 +0530

    verify email

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,22 +1,23 @@
-// utils/sendEmail.js
 import SibApiV3Sdk from "@sendinblue/client";
 import "dotenv/config";
 
-export const sendEmail = async (to, htmlContent, subject) => {
+export const sendEmail = async (to, subject, htmlContent) => {
   try {
     const client = new SibApiV3Sdk.TransactionalEmailsApi();
-    client.setApiKey(SibApiV3Sdk.TransactionalEmailsApiApiKeys.apiKey, process.env.BREVO_API_KEY);
+    client.setApiKey(
+      SibApiV3Sdk.TransactionalEmailsApiApiKeys.apiKey,
+      process.env.BREVO_API_KEY
+    );
 
-    await client.sendTransacEmail({
+    const response = await client.sendTransacEmail({
       sender: { email: process.env.EMAIL_USER, name: "HappyTails" },
       to: [{ email: to }],
       subject,
       htmlContent,
     });
-
-    console.log("✅ Email sent successfully to:", to);
+    console.log("✅ Email sent successfully to:", to, response);
   } catch (err) {
-    console.error("❌ Email sending failed:", err);
+    console.error("❌ Email sending failed:", err.response?.body || err);
     throw new Error("Email sending failed");
   }
 };
